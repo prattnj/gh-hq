9f415d2aafda9ea26364195cec80f969cb6dcb91
Noah Pratt <prattnj@gmail.com>
2023-02-10 22:16:13 -0700

    slideshow 3

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 async function changeBackground() {
     let images = ["url('images/furniture.jpg')", "url('images/food.jpg')", "url('images/monks.jpg')", "url('images/amogus.jpg')"]
+    preloadImages(images)
     let i = 0
     while (1 === 1) {
         document.getElementById("large-bg-img").style.backgroundImage = images[i]
         if (i === 3) i = 0
         else i++
-        await sleep(2000)
+        await sleep(4000)
+    }
+}
+
+function preloadImages(images) {
+    for (let i = 0; i < images.length; i++) {
+        let img = new Image()
+        img.src = images[i]
     }
 }
 
