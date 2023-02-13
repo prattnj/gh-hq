8d4859cb3019c88d6c2bb0af65318834d05ee36c
Noah Pratt <prattnj@gmail.com>
2023-02-13 14:21:15 -0700

    slideshow 4

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ async function changeBackground(images) {
     let i = 0
     while (1 === 1) {
         document.getElementById("large-bg-img").style.backgroundImage = images[i]
-        if (i === 3) i = 0
+        if (i === images.length - 1) i = 0
         else i++
         await sleep(1000)
     }
